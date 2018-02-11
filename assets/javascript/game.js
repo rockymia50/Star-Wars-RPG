38e0b3939c463d6432b688d8949c1118a60d8b4f
Roger McIntosh <rogermcintosh@Rogers-Air.home>
2018-02-11 08:50:12 -0500

    update

---
+++ assets/javascript/game.js
@@ -0,0 +1,43 @@
+
+
+
+let chew = {
+    health: 100,
+    attack:8,
+    counter: 20
+
+}
+
+let darth = {
+    health: 150,
+    counter: 20
+
+
+
+}
+
+let storm = {
+    health: 180,
+    counter: 25
+
+
+}
+
+let dark = {
+    health: 100,
+    counter: 5
+
+}
+
+const printToScreen = () => {
+    document.getElementsById("chew").innerText = chew.health;
+
+    const printToScreen = () => 
+        document.getElementsById("darth").innerText = darth.health;
+    
+        const printToScreen = () => 
+        document.getElementsById("storm").innerText = storm.health;
+    
+        const printToScreen = () => 
+        document.getElementsById("dark").innerText = dark.health;
+}
